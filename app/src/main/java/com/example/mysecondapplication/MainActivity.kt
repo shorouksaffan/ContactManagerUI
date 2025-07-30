03e05eaa34bcc4b8ff1da73184170fff3e669375
shorouksaffan <shorouk.saffan2005@gmail.com>
2025-07-30 22:56:15 +0300

    Initial commit for my second task

---
+++ app/src/main/java/com/example/mysecondapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mysecondapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        }
+}
